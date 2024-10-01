1ac7486dc68e7763fc779d60dc21da7a4db12f63
OTOOOOOOOOOOOO <147213418+OTOOOOOOOOOOOO@users.noreply.github.com>
2024-10-01 20:42:32 +0700

    Add files via upload

---
+++ Perbaikan_Nilai/src/LuasSegitiga.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class LuasSegitiga {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan panjang alas segitiga: ");
+        double alas = scanner.nextDouble();
+
+        System.out.print("Masukkan tinggi segitiga: ");
+        double tinggi = scanner.nextDouble();
+
+        double luas = (alas * tinggi) / 2 ;
+
+        System.out.println("Luas segitiga adalah: " + luas);
+
+        scanner.close();
+    }
+}
